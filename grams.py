c4a5267c2711812b68aa9b536a04cf6930ec88b8
Daniel <thevonster4@gmail.com>
2024-08-14 11:16:20 -0400

    Update grams.py

--- grams.py
+++ grams.py
@@ -1,7 +1,7 @@
 import improvedmw
 
-def main(formula, moles):
-    mw = float(improvedmw.main(formula))
+def main(formula, moles, lib):
+    mw = float(improvedmw.main(formula, lib))
     grams = mw * float(moles)
     grams = round(grams, 3)
     return grams
