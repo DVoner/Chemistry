fa17319e08e42952a2f48d266bf17a1faf5f3334
Daniel <thevonster4@gmail.com>
2024-08-15 10:37:32 -0400

    Update moles.py

--- moles.py
+++ moles.py
@@ -2,6 +2,6 @@ import improvedmw
 
 def main(formula, grams, lib):
     mw = float(improvedmw.main(formula, lib))
-    moles = mw * (1 / float(grams))
+    float(grams) = mw * (1 / moles)
     moles = round(moles, 3)
     return moles
